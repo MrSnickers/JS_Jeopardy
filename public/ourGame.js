109b492be8fff145e92f2cf9f82ed0bba4319f24
Sterling Walker <lazarus_star@hotmail.com>
2014-01-30 19:14:08 -0500

    Prevents choosing more than one question at a time.

--- public/ourGame.js
+++ public/ourGame.js
@@ -3,11 +3,14 @@
 //cards should be clickable but catagories should not be
 
 $(".value").on("click", function (){
+  $(this).parent().siblings().removeClass("chosen");
   $(this).parent().addClass("chosen");
-  $(".value").not(this).parent().toggleClass("disabled");
-  $(this).parent().addClass("question");
+  $(".value").not(this).parent().siblings().removeClass("disabled");
+  $(".value").not(this).parent().addClass("disabled");
 });
-
+$(".chosen").on("click", function(){
+  
+});
 
 $(".chosen").on("click", function (){
   var $this = $(this);
