3d96e22cc84389396ed1c42def26b3344d0435c2
Sterling Walker <lazarus_star@hotmail.com>
2014-02-16 18:03:27 -0500

    Image info included.

--- public/webcomic_memory.js
+++ public/webcomic_memory.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
+  var $card = $("div.card");
+
+  $card.on("click", function(){
+    $(this).addClass("chosen");
+    $(this).find("img.front").fadeIn();
+    $(this).find(".info").fadeIn();
+  })
 
 
 
 
 
 
-
-
-
-  
 })
