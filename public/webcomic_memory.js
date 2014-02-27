daeccfc4d19a70032ab7b8e5bcf97c04dcf9faab
Sterling Walker <lazarus_star@hotmail.com>
2014-02-27 17:19:02 -0500

    Knows when match is not made.

--- public/webcomic_memory.js
+++ public/webcomic_memory.js
@@ -1,15 +1,48 @@
 $(document).ready(function(){
-  var $card = $("div.card");
 
-  $card.on("click", function(){
-    $(this).addClass("chosen");
-    $(this).find("img.front").fadeIn();
-    $(this).find(".info").fadeIn();
-  })
+  var $cardOne;
+  var $cardTwo;
+
+  $("div.card").on("click", function(event){
+
+    event.stopPropagation();
+    event.preventDefault();
+
+    function select($card){
+      $card.addClass("chosen");
+      flip($card);
+
+    };
+
+    function flip($card){
+      $card.find(".back").toggle();
+      $card.find(".front").toggle(); 
+    }
+
+    if(!$cardOne){
+      $cardOne = $(this);
+      select($cardOne);
+    }else{
+      if(!$cardTwo){
+        $cardTwo = $(this);
+        select($cardTwo);
+        if ($cardOne.find(".info").text() === $cardTwo.find(".info").text()){
+          $cardOne.removeClass("chosen").addClass("matched");
+          $cardTwo.removeClass("chosen").addClass("matched");
+          $cardOne = undefined;
+          $cardTwo = undefined;
+        }else{
+          $cardOne.removeClass("chosen");
+          flip($cardOne);
+          $cardTwo.removeClass("chosen");
+          flip($cardTwo);
+          $cardOne = undefined;
+          $cardTwo = undefined;
+        }
+      }
+    }
 
 
+  });
 
-
-
-
-})
+});
